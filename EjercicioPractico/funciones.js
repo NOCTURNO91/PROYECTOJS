71b8ca3e89feede3d2b1636dbea8899d0987346a
carlos quiroz <cequiroz@tcc.com.co>
2023-10-18 11:50:18 -0500

    trabajos js

---
+++ EjercicioPractico/funciones.js
@@ -0,0 +1,17 @@
+function mostrarTexto(){
+    alert(document.getElementById(inputTexto).value);
+
+    let pelicula = "Drestroyer";
+    let hora = "22.00";
+}
+
+function text(){
+    alert("La funcion de" + pelicula + "es a alas " + pelicula);
+}
+
+let fruta = "manzana";
+let precio = "1000";
+
+function text(){
+    alert("Las" + frutas + "cuestan" + precio);
+}
